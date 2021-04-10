c1520bef47adeecb83896a2564f3e04db9057853
SakshyamShrestha <np03a190041@heraldcollege.edu.np>
2021-04-10 17:16:28 +0545

    adding url routing

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,15 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+class LoginPageView(TemplateView):
+    template_name = 'login.html'
+
+class RegFormView(TemplateView):
+    template_name = 'regform.html'
+
+class CategoriesView(TemplateView):
+    template_name = 'categories.html'
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
