8dc4eba26fe18904946ff18e3ad81e62fc7ce28e
akinstha04 <np03a190061@heraldcollege.edu.np>
2021-04-14 11:00:01 +0545

    backend file

--- pages/urls.py
+++ pages/urls.py
@@ -1,12 +1,10 @@
 # pages/urls.py
 
 from django.urls import path
-from .views import HomePageView, RegFormView, LoginPageView, CategoriesView, ContactView
+from .views import HomePageView, CategoriesView, ContactView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('login/', LoginPageView.as_view(), name='login'),
-    path('registration/', RegFormView.as_view(), name='regform'),
     path('categories/', CategoriesView.as_view(), name='categories'),
     path('contact/', ContactView.as_view(), name='contact'),
 ]
