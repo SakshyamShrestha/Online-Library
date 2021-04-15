e9c87e2327ffcace18be2eeccd26dd00754f15e1
SakshyamShrestha <np03a190041@heraldcollege.edu.np>
2021-04-15 14:13:01 +0545

    email field bug

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        res = self.client.get('/')
+        self.assertEqual(res.status_code, 200)
