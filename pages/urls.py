a02bab56ee36e89e80d3ee33b57523ed6a23897e
SushantPokharel316 <np03a190316@heraldcollege.edu.np>
2021-05-11 15:13:36 +0545

    added a request book page

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,12 @@
 # pages/urls.py
 
 from django.urls import path
-from .views import HomePageView, CategoriesView, ContactView
+from .views import HomePageView, CategoriesView, ContactView, BookreqView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('categories/', CategoriesView.as_view(), name='categories'),
     path('contact/', ContactView.as_view(), name='contact'),
+    path('bookreq/', BookreqView.as_view(), name='bookreq'),
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,16 +1,43 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
-from .models import Page
+from .models import Categories
+from .models import Contact
 from django.views.generic import ListView
+
+
+# from django.http import HttpResponse
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'pages/index.html'
 
+
 class CategoriesView(ListView):
-    model = Page
+    model = Categories
     template_name = 'pages/categories.html'
     context_object_name = 'all_pages_list'
 
+class BookreqView(TemplateView):
+    template_name = 'pages/bookreq.html'
+
 class ContactView(TemplateView):
+    model = Contact
     template_name = 'pages/contact.html'
+
+
+# def contact(request):
+#     if request.method == "POST":
+#         contact = Contact()
+#         name = request.POST.get('name')
+#         email = request.POST.get('email')
+#         message = request.POST.get('message')
+#         contact.name = name
+#         contact.email = email
+#         contact.message = message
+#         contact.save()
+#         return HttpResponse ("<h2>Thanks for contacting us</h2>")
+#     return render(request, 'pages/contact.html')
+
+# def post(self, request):
+#     form = HomeForm(request.POST)
+#     if form.is_valid():
+#         text = form.cleaned_data('post')
