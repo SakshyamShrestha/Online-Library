bf4ac50c0c83114b23dedaafc6153a8a17d9e295
SushantPokharel316 <np03a190316@heraldcollege.edu.np>
2021-05-12 13:39:56 +0545

    admin changes for categories

--- pages/admin.py
+++ pages/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Page 
+from .models import Categories
+from .models import Contact
 
 # Register your models here.
 
-admin.site.register(Page)
+admin.site.register(Categories)
+
+
