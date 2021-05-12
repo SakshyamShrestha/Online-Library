968c9c1ebc482101c7fe77fbc028dbeadda73ed0
SushantPokharel316 <np03a190316@heraldcollege.edu.np>
2021-05-12 19:35:20 +0545

    Model and Views updated

--- pages/models.py
+++ pages/models.py
@@ -16,3 +16,11 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+class Books(models.Model):
+    title = models.CharField(unique=True, max_length=200)
+    image = models.ImageField(upload_to = "images/", default= 'test')
+    category = models.CharField(max_length = 200, default= "miscellaneous")
+
+    def __str__(self):
+        return self.title
--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,20 @@
 from django.views.generic import TemplateView
 from .models import Categories
 from .models import Contact
+from .models import Books
 from django.views.generic import ListView
 
 
 # from django.http import HttpResponse
 
 # Create your views here.
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Books
+    # books = Book.objects.all()
     template_name = 'pages/index.html'
+    context_object_name = 'all_pages_list'
+    # context_object_name = 'book'
+    # queryset = Book.object.all()
 
 
 class CategoriesView(ListView):
@@ -16,28 +22,24 @@ class CategoriesView(ListView):
     template_name = 'pages/categories.html'
     context_object_name = 'all_pages_list'
 
+
 class BookreqView(TemplateView):
     template_name = 'pages/bookreq.html'
+
 
 class ContactView(TemplateView):
     model = Contact
     template_name = 'pages/contact.html'
 
-
-# def contact(request):
-#     if request.method == "POST":
-#         contact = Contact()
-#         name = request.POST.get('name')
-#         email = request.POST.get('email')
-#         message = request.POST.get('message')
-#         contact.name = name
-#         contact.email = email
-#         contact.message = message
-#         contact.save()
-#         return HttpResponse ("<h2>Thanks for contacting us</h2>")
-#     return render(request, 'pages/contact.html')
-
-# def post(self, request):
-#     form = HomeForm(request.POST)
-#     if form.is_valid():
-#         text = form.cleaned_data('post')
+def contact(request):
+    if request.method == "POST":
+        contact = Contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        contact.name = name
+        contact.email = email
+        contact.message = message
+        contact.save()
+        return HttpResponse ("<h2>Thanks for contacting us</h2>")
+    return render(request, 'pages/contact.html')
