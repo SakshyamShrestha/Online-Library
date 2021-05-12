9b3b6b6ab618be359d92ce33ee93f9064c282b84
SushantPokharel316 <np03a190316@heraldcollege.edu.np>
2021-05-12 19:39:07 +0545

    admin.py updated

--- pages/admin.py
+++ pages/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import Categories
+from .models import Books
 from .models import Contact
 
 # Register your models here.
 
 admin.site.register(Categories)
+admin.site.register(Books)
 
-
+@admin.register(Contact)
+class Contact(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'message')
