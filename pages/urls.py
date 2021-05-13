33151b0715fa2b5d9e32b0963714b63ee9e4e77a
SushantPokharel316 <np03a190316@heraldcollege.edu.np>
2021-05-13 09:53:27 +0545

    page urls updated

--- pages/urls.py
+++ pages/urls.py
@@ -1,12 +1,13 @@
-# pages/urls.py
-
 from django.urls import path
-from .views import HomePageView, CategoriesView, ContactView, BookreqView
+from .views import HomePageView, CategoriesView, ContactView, BookreqView, FaqView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('categories/', CategoriesView.as_view(), name='categories'),
+    # path('<slug:slug>/', Categorie ).
     path('contact/', ContactView.as_view(), name='contact'),
+    path('faq/', FaqView.as_view(), name='faq'),
     path('bookreq/', BookreqView.as_view(), name='bookreq'),
+    
 
 ]
