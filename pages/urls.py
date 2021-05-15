154b87b52f8b52e0f6c44008ba690dfa30f3ad2e
SushantPokharel316 <np03a190316@heraldcollege.edu.np>
2021-05-15 18:23:23 +0545

    urls updated

--- pages/urls.py
+++ pages/urls.py
@@ -4,9 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    # path('categories/', CategoriesView.as_view(), name='categories'),
     path('categories/', views.categories, name='categories'),
-    path('categories/<slug:slug>/', views.category, name='category'), 
     path('contact/', views.ContactView, name='contact'),
     path('faq/', FaqView.as_view(), name='faq'),
     path('bookreq/', views.BookreqView, name='bookreq'),
