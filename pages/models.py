e0a0678b5e146f46d9c7f66320209d6e12c137a3
SushantPokharel316 <np03a190316@heraldcollege.edu.np>
2021-05-15 14:35:58 +0545

    object title bug fixed

--- pages/models.py
+++ pages/models.py
@@ -3,13 +3,10 @@ from django.db import models
 # Create your models here.
 class Categories(models.Model):
     text = models.CharField(unique=True, max_length=200)
-    slug = models.SlugField(max_length = 200, default= "miscellaneous")
 
     def __str__(self):
         return self.text
 
-    # def get_absolute_url(self):
-    #     return reverse('article_detail', kwargs={'slug': self.slug})
 
 class Contact(models.Model):
     name = models.CharField(max_length=200)
